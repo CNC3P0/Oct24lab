6849a0e097111e9c1d6574dddb2c8c4c6100b3d5
CNC3P0 <rspanell@hotmail.com>
2017-10-24 19:33:30 -0700

    working

--- app/src/main/java/com/example/t00597013/myapplication/MainActivity.java
+++ app/src/main/java/com/example/t00597013/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.t00597013.myapplication;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,8 +12,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     public int pass = 12345;
-    public int range = 10000000;
-    private int guess = 0;
+    public int range = 10000;
     TextView progressText;
     ProgressBar progressBar;
 
@@ -23,19 +23,44 @@ public class MainActivity extends AppCompatActivity {
 
         progressText = (TextView) findViewById(R.id.progressText);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-
-
     }
 
     public void go(View view) {
         progressText.setText("Calculating...");
         progressBar.setProgress(2);
-        Random random = new Random();
-        while (guess != pass) {
-            guess = random.nextInt() % range;
+
+        new Potato().execute(range, pass);
+    }
+
+    private class Potato extends AsyncTask<Integer, Integer, String> {
+
+        private int guess = 0;
+        private int count = 0;
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+            int theRange = integers[0];
+            int thePass = integers[1];
+            Random random = new Random();
+            while (guess != thePass) {
+                guess = random.nextInt() % theRange;
+                count += 1;
+                publishProgress((int)count * 100 / theRange);
+            }
+            return "Found pass: " + guess;
         }
 
-        progressText.setText("Found pass: " + guess);
-        progressBar.setProgress(100);
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressBar.setProgress((int)(values[0] + 100.0 / range * 3));
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressBar.setProgress(100);
+            progressText.setText(s);
+        }
     }
 }
